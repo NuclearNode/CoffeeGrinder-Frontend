5c0e4d385fe03303d2f9610736f0453b7fd8bf80
Smith <pentopia11@gmail.com>
2020-10-09 01:47:29 -0700

    moved swipe stuff to other files for later. fixed footer problem. in the process of formatting the drink interest quiz.

--- quizscript.js
+++ quizscript.js
@@ -1,32 +1,10 @@
-$(document).ready(function() {
-    var animating = false;
-    var decisionVal = 80;
-    var pullDeltaX = 0;
-    var deg = 0;
-    var $choice, $choiceAccept;
-    var $left, $right;
-    
-    function pullChange() {
-        animating = true;
-        deg = pullDeltaX/10;
-        $choice.css("transform", "translateX("+ pullDeltaX +"px) rotate("+ deg +"deg)");
-    }
-    
-    function release() {
-        if (pullDeltaX >= decisionVal && right) {
-            $choice.addClass("to-right");
-        } else if (pullDeltaY <= -decisionVal && left) {
-            $choice.addClass("to-left");
-        }
-        
-        if (Math.abs(pullDeltaX) < decisionVal) {
-            $choice.addClass("reset");
-        }
-        
-        setTimeout(function() {
-            $choice.attr("style", "").removeClass("reset").find(".")
-        })
-    }
-    
-    
-});
+function hide(questionId) {
+//    document.getElementById(questionId).classList.add("hide");
+//    document.getElementById(questionId).classList.remove("show");
+    document.getElementById(questionId).style.display = none;
+}
+
+function show(questionId) {
+    document.getElementById(questionId).classList.add("show");
+    document.getElementById(questionId).classList.remove("hide");
+}
