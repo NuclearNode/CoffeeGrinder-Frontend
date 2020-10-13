363fbbdedf81010a1eb5e3c8c0bdd778df037839
Smith <pentopia11@gmail.com>
2020-10-13 08:35:50 -0700

    quiz cycle

--- quizscript.js
+++ quizscript.js
@@ -1,13 +1,36 @@
-document.getElementByClassName("n1").addEventListener("click", function(event) {
-    event.preventDefault();
-});
+// https://stackoverflow.com/questions/34878569/how-to-store-check-box-values-using-javascript-or-jquery
 
-function hide(questionId) {
-    document.getElementById(questionId).classList.remove("show");
-    document.getElementById(questionId).classList.add("hide");
+$('#allergies_confirm').click(function() {
+    alert($('input').serialize());
+})
+
+// look i don't really know how to make sure this is working in any expedient way, but since it's not important for the design right now, i'm gonna leave this hanging
+// we also need to figure out how to store this information locally so that it doesn't disappear on reload
+
+
+var path = ["hotcold"];
+
+var types = ["hotespresso", "hotnoespresso", "hotfruity", "hotnofruity", "hotchocolate", "hotjuice", "hotsteamer", "coldcaramelfrap", "coldmochafrap", "coldcoffeefrap", "coldchocolatefrap", "coldnochocolatefrap", "coldespresso", "coldnoespresso", "coldfruity", "coldnofruity", "coldrefresher", "coldcoconutmilk"]
+
+
+function hide(pageID) {
+    document.getElementById(pageID).style.display = "none";
 }
 
-function show(questionId) {
-    document.getElementById(questionId).classList.remove("hide");
-    document.getElementById(questionId).classList.add("show");
+function register(pageID) {
+    path.push(pageID);
+}
+
+function goto(pageID) {
+    document.getElementById(pageID).style.display = "block";
+}
+
+function goBack() {
+    var currentPage = path.pop();
+    if (types.includes(currentPage)) {
+        currentPage = "sugar";
+    }
+    document.getElementById(currentPage).style.display = "none";
+    var currentPage = path[path.length - 1];
+    document.getElementById(currentPage).style.display = "block";
 }
