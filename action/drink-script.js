7cedc2962adc3030b9b7721c716f17d8b1630a53
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-11-18 16:23:38 -0800

    dynamic display

--- action/drink-script.js
+++ action/drink-script.js
@@ -1,14 +1,61 @@
+let temp = false;
+let type = 'None';
+
+console.log(temp);
+
+
+function updateHot()
+{
+    console.log(temp);
+    temp = true;
+    console.log(temp);
+}
+
+
+function updateCold()
+{
+    console.log(temp);
+    temp = false;
+    console.log(temp);
+}
+
+function updateCoffee()
+{
+    console.log(type);
+    type = 'Coffee';
+    console.log(type);
+}
+
+function updateTea()
+{
+    console.log(type);
+    type = 'Tea'
+}
+
+function updateFrap()
+{
+    console.log(type);
+    type = 'Frappuccino'
+    console.log(type);
+}
+
+
 fetch('drinks.json').then(function(response) {
   return response.json();
 }).then(function(json) {
     let drinks = json;
     console.log(drinks.length);
+    
     for(var i = 0;i<drinks.length;i++)
     {
         var drink = drinks[i];
         console.log(drink.name);
     }
     
+    
+   
+    
+    /*displays the drinks json*/
     document.getElementById("results").innerHTML = 
         `
         <h1>${drinks.length} drinks suggested</h1>
