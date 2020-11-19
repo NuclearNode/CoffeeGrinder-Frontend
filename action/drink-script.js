bd4039ac32664635af3fbcec5530baa7c606a728
Jeffrey McDonald <jffrmcdonald@gmail.com>
2020-11-18 19:15:37 -0800

    fixed fetch

--- action/drink-script.js
+++ action/drink-script.js
@@ -2,10 +2,6 @@ let temp = false;
 let type = 'None';
 let espresso = false;
 let fruity = false;
-<<<<<<< HEAD
-let category = 'None';
-let sugar = -1;
-=======
 let coconut_milk = false;
 let category = 'None';
 let wheat = false;
@@ -17,7 +13,6 @@ let secondSugar = 1.5;
 let thirdSugar = 2.5;
 
 
->>>>>>> 3c8c6278addd0df7340da7cc08d7d2a24aaa77e4
 console.log(temp);
 
 
@@ -92,92 +87,6 @@ function noFruity()
     console.log(fruity);
 }
 
-<<<<<<< HEAD
-function updateSugar1()
-{
-    sugar = .5;
-}
-
-function updateSugar2()
-{
-    sugar = 2.1;
-}
-
-function updateSugar3()
-{
-    sugar = 3.2;
-}
-
-function updateAmericano()
-{
-    category = 'Americano';
-}
-
-function updateBlackTea()
-{
-    category = 'Black';
-}
-
-function updateBrewed()
-{
-    category = 'Brewed';
-}
-
-function updateCappuccino()
-{
-    category = 'Cappuccino';
-}
-
-function updateCoffeeFrap()
-{
-    category = 'Coffee Frappuccino';
-}
-
-function updateColdBrew()
-{
-    category = 'Cold Brew';
-}
-
-function updateCremeFrap()
-{
-    category = 'Creme Frappuccino';
-}
-
-function updateEspressoShot()
-{
-    category = 'Espresso Shot';
-}
-
-function updateFlatWhite()
-{
-    category = 'Flat White';
-}
-
-function updateGreenTea()
-{
-    category = 'Green';
-}
-
-function update
-
-function updateHotChocolate()
-{
-    console.log(category);
-    category = 'Hot Chocolate';
-    console.log(category);
-}
-
-function updateRefresher()
-{
-    category = 'StarbucksRefresher';
-}
-
-function updateIcedCoconutMilk()
-{
-    category = 'IcedCoconutMilk';
-}
-
-=======
 function yesRefresher()
 {
 	console.log(category);
@@ -282,53 +191,17 @@ function yesSteamer()
 	category = 'Steamer';
 	console.log(category);
 }
->>>>>>> 3c8c6278addd0df7340da7cc08d7d2a24aaa77e4
 
 function firstSweet()
 {
 	
+}
 
 fetch('drinks.json').then(function(response) {
   return response.json();
 }).then(function(json) {
-    let drinks = json;
-    console.log(drinks.length);
-    
-    let final_order = []
-    for(var i = 0;i<drinks.length;i++)
-    {
-        if(drinks[i].type=='Tea')
-            final_order.push(drinks[i]);
-        var drink = drinks[i];
-        console.log(drink.name);
-    }
-    
-    
-   
-    
-    /*displays the drinks json*/
-    document.getElementById("results").innerHTML = 
-        `
-        <h1>${final_order.length} drinks suggested</h1>
-        
-        ${final_order.map(function(drink){
-        return `<div class="card">
-                    <img class="drink-photo" src="${drink.image}">
-                    <h1>${drink.name}</h1>
-                    <p>${drink.description}</p>
-                </div>
-        `
-    }).join('')}
-
-        `
-    
-   
-        
-  
+  let drinks = json;
+  ;
 }).catch(function(err) {
   console.log('Fetch problem: ' + err.message);
-  console.log(path.dir);
 });
-
-
-
